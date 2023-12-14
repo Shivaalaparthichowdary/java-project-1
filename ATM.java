7bf5f728902166fe48c26a7420e002eaaeefe638
Shivaalaparthichowdary <136696445+Shivaalaparthichowdary@users.noreply.github.com>
2023-12-14 10:54:40 +0530

    Add files via upload
    Basic atm project

---
+++ ATM.java
@@ -0,0 +1,9 @@
+package Shiva;
+import java.io.IOException;
+public class ATM extends Optionmenu{
+	
+public static void main(String[] args) throws IOException{
+Optionmenu obj =new Optionmenu();
+obj.getlogin();
+}
+}
