bacf3909a2854019483882d81b0e6c57014a749f
Shivaalaparthichowdary <136696445+Shivaalaparthichowdary@users.noreply.github.com>
2023-12-14 10:59:21 +0530

    Delete ATM.java

--- ATM.java
+++
@@ -1,9 +0,0 @@
-package Shiva;
-import java.io.IOException;
-public class ATM extends Optionmenu{
-	
-public static void main(String[] args) throws IOException{
-Optionmenu obj =new Optionmenu();
-obj.getlogin();
-}
-}
